4a65c66b54b392f7b09e606a2a0e4cac0afa7f2d
shamingaa <shamingaa@gmail.com>
2026-06-02 06:55:02 +0100

    Just a change

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001',
 });
 
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('admin_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
